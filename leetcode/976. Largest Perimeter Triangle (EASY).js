ee5ed22059b0d885ece8c4a696a0de0374403917
taranpalsingh <taranpal.singh23@gmail.com>
2022-10-12 01:35:23 -0400

    daily

---
+++ leetcode/976. Largest Perimeter Triangle (EASY).js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var largestPerimeter = function(nums) {
+    
+  nums= nums.sort((a,b) => a-b);
+  for (let i=nums.length-1; i>=2; i--) {
+      if (nums[i-1] + nums[i-2] > nums[i])
+          return nums[i] + nums[i-1] + nums[i-2]; 
+  }
+
+  return 0;
+};

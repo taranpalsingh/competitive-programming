dc6f71205c1bb722dfedebf9b9f9d568725422f8
taranpalsingh <taranpal.singh23@gmail.com>
2022-10-13 00:13:31 -0400

    daily

---
+++ leetcode/237. Delete Node in a Linked List (MED).js
@@ -0,0 +1,19 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} node
+ * @return {void} Do not return anything, modify node in-place instead.
+ */
+var deleteNode = function(node) {
+  while (node.next) {
+    node.val = node.next.val;
+    prev = node;
+    node = node.next;
+  }
+  prev.next = null;
+};
